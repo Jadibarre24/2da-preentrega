57fb9f35fa2bbbdfe960968bbd983f3480269d62
jadibarre24 <andy024.jd@gmail.com>
2024-04-14 22:14:53 -0500

    html-listo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-const items = document.getElementsByClassName("button")
-
-console.log(items)
+const productos1 = document.getElementById ("Altavoces") 
+const productos2 = document.getElementById ("Microfonos")
+const productos3 = document.getElementById ("Bases")
+const productos4 = document.getElementById ("Soportes")
+const productos5 = document.getElementById ("Luces")
+const productos6 = document.getElementById ("Consolas")
+const productos = [productos1, productos2,productos3,productos4,productos5,productos6]
+console.log(productos)
