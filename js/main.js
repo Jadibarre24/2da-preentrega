0f66b883cfb7443495448b61ca54a0289514ebe8
jadibarre24 <andy024.jd@gmail.com>
2024-04-15 06:13:07 -0500

    logica-boton

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
-const productos1 = document.getElementById ("Altavoces") 
-const productos2 = document.getElementById ("Microfonos")
-const productos3 = document.getElementById ("Bases")
-const productos4 = document.getElementById ("Soportes")
-const productos5 = document.getElementById ("Luces")
-const productos6 = document.getElementById ("Consolas")
-const productos = [productos1, productos2,productos3,productos4,productos5,productos6]
-console.log(productos)
+let inventario = document.getElementsByClassName  ("card-item")
+const Altavoces = document.getElementById ("Altavoces") 
+const Microfonos = document.getElementById ("Microfonos")
+const Bases = document.getElementById ("Bases")
+const Soportes = document.getElementById ("Soportes")
+const Luces = document.getElementById ("Luces")
+const Consolas = document.getElementById ("Consolas")
+const productos = [Altavoces, Microfonos,Bases,Soportes,Luces,Consolas]
+const agregarItem = document.getElementsByClassName ("button")
+const agregarItemarray = Array.from(agregarItem)
+agregarItemarray.forEach((button)=>{button.addEventListener("click",(e)=>{
+    console.log(e.target.parentElement.children[0].innerText)
+})})
+console.log(agregarItemarray)
