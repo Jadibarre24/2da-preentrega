adb6f822efd75d7d4538426bf7996dfcb74321bf
Jadibarre24 <andy024.jd@gmail.comgit config --listgit config --user.emailgit git config --global user.emailandy024.jd@gmail.com>
2024-05-20 10:42:46 -0500

    JsonTEst

--- js/main.js
+++ js/main.js
@@ -1,51 +1,120 @@
-// let inventario = document.getElementsByClassName  ("card-item")
-// const Altavoces = document.getElementById ("Altavoces") 
-// const Microfonos = document.getElementById ("Microfonos")
-// const Bases = document.getElementById ("Bases")
-// const Soportes = document.getElementById ("Soportes")
-// const Luces = document.getElementById ("Luces")
-// const Consolas = document.getElementById ("Consolas")
-// const productos = [Altavoces, Microfonos,Bases,Soportes,Luces,Consolas]
 
-// const agregarItem = document.getElementsByClassName ("button")
-// const addItems = []
+const productos = ["Altavoces", "Microfonos"]
+const microfonos=["Shure","Senheisser","Rode","Berhinger"]
 
-// const refreshCar =() =>{
-//     agregarItem.forEach((agregarItem)=>{
-//         addItems.innerHTML += agregarItem
-//     }
-// )
-// }
+class Altavoces{
+    constructor(marca,referencia,precio){
+        this.marca = marca;
+        this.referencia = referencia;
+        this.precio = precio;
+    }
+}
+const Altavoz1 = new Altavoces ("JBL","Eon","2`500.000")
+const Altavoz2 = new Altavoces ("JBL","Subwoofer","3`250.000")
+const altavoces=[Altavoz1,Altavoz2,]
 
-// const agregarItemarray = Array.from(agregarItem)
+let AltavocesEnTexto = JSON.stringify("altavoces")
 
-// agregarItemarray.forEach((button)=>{
-//     button.addEventListener("click",(e)=>{
-//         const ItemContainer= e.target.parentElement.children[0].innerText
-//         addItems.push(ItemContainer)
-//         refreshCar()
-//     })
+localStorage.getItem("altavoces",AltavocesEnTexto)
 
-// })
+console.log(Altavoz1,Altavoz2)
+console.log(AltavocesEnTexto)
 
-// console.log(agregarItemarray)
-const inventario = document.querySelectorAll(".card-item");
-const productos = document.querySelectorAll(".button");
-const addItems = [];
-const Car = document.getElementById("carrito")
+let AltavocesLook = JSON.parse(localStorage.getItem(altavoces))
 
-const chargercar = () =>{
-    Car.innerHTML = " "
-    addItems.forEach((productos)=>{
-        Car.innerHTML += productos
+
+
+class Microfonos{
+    constructor(marca,referencia,precio){
+        this.marca = marca
+        this.referencia = referencia;
+        this.precio = precio;
+    }
+}
+const ContainerAltavoces = document.getElementById("altavoces") 
+const ContainerMicrofocos = document.getElementById("microfonos")
+
+altavoces.forEach((producto) => {
+    ContainerAltavoces.innerHTML += `
+    <div class="card-item">
+    <h2> ${producto}</h2>
+    <button class="addCar">Agregar a carrito</button>
+    </div>` 
+}); 
+microfonos.forEach((producto) => {
+    ContainerMicrofocos.innerHTML += `
+    <div class="card-item">
+    <h2> ${producto}</h2>
+    <button class="addCar">Agregar a carrito</button>
+    </div>` 
+}); 
+
+/* function verProductos(){  
+    productos.altavoces.forEach((producto) => {
+        Container.innerHTML += `
+        <div>
+        <h2> ${producto}</h2
+        
+        </div>` 
+    });  
+}
+function verProductos2(){  
+    productos.Microfonos.forEach((producto) => {
+        Container.innerHTML += `
+        <div>
+        <h2> ${producto}</h2
+        
+        </div>` 
+    });  
+} */
+
+/* const botonVer = document.getElementsByClassName("botonVer")
+const botonVerArray = Array.from(botonVer)
+botonVerArray.forEach((ver) => {
+    ver.addEventListener("click",(e)=>{
+        verProductos()
+        console.dir(e.target.parentElement.children[0].innertext)
     })
 }
+) */
 
-productos.forEach((button) => {
-    button.addEventListener("click", (e) => {
-        const ItemContainer = e.target.parentElement.innerHTML;
-        addItems.push(ItemContainer);
-        chargercar()
-        console.log(e);
-    });
-});
+
+/* productos.altavoces.forEach((item) => {
+    Container.innerHTML += `
+    <div>
+    <h2> ${item}</h2>
+    <button class="botonVer">ver</button>
+    </div>` 
+});  */
+/* verProductos()  */
+   /*  const Container = document.createElement("div");
+    contenedor.innerText= producto.producto;
+    const contenedor1 = document.createElement("div");
+    const item = document.createElement("h3");
+    item .innerText=productos.altavoces
+    contenedor1.appendChild(item);
+
+/* productos.altavoces.forEach((item) => {
+    ItemContainer.innerHTML += `
+    
+    
+    <li>
+            ${item}
+            </li>
+        `
+    
+}); */
+
+/* 
+const contenedor = document.createElement("div");
+const titulo = document.createElement("h2");
+const botonAgregar = document.createElement("button");
+
+titulo.textContent = productos
+botonAgregar.innerText = "agregar"
+
+contenedor.appendChild (titulo);
+contenedor.appendChild (botonAgregar); */
+/* console.log(altavoces) */
+
+    
